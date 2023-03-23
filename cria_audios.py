a72bfc466f4506e1d17c09f7cd8d053f35ce12c8
Bruno Oliveira <brunoricardo@brunoricardo.me>
2023-03-23 10:41:07 -0300

    Feature: Play nos audios

--- cria_audios.py
+++ cria_audios.py
@@ -1,4 +1,16 @@
-from gtts import gTTS
+from gtts import gTTS ## Para criar os audios
+from subprocess import call ## Para executar o comando de reprodução no mac e linux
+#from playsound import playsound ## Para executar o comando de reprodução no windows
 
-tts = gTTS(text='Oi, eu sou a Ava', lang='pt-br')
-tts.save('audios/oi.mp3')
+def cria_audio(audio):
+    tts = gTTS(audio, lang='pt-br') ## Cria o audio
+    tts.save('audios/'+ audio + '.mp3') ## Salva o audio
+
+    call(["afplay", "audios/" +audio+ ".mp3"]) ## Executa o comando de reprodução no mac
+    #call(["aplay", "audios/oi.mp3"]) ## Executa o comando de reprodução no linux
+    #call(["cmdmp3", "audios/oi.mp3"]) ## Executa o comando de reprodução no windows
+    #playsound('audios/oi.mp3') ## Executa o comando de reprodução no windows
+
+cria_audio('Olá! Tudo bem?') # Chama a função
+cria_audio('Bem vindo') # Chama a função
+
